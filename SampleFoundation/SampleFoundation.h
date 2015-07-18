7677648ebb155d09c1718f0283fa4e1b14fc5745
Pidhi <pidhi@Pidhis-Mac-mini.local>
2015-07-18 10:41:13 +0530

    Initial commit of library
    Signed-off-by: Pidhi <pidhi@Pidhis-Mac-mini.local>

---
+++ SampleFoundation/SampleFoundation.h
@@ -0,0 +1,13 @@
+//
+//  SampleFoundation.h
+//  SampleFoundation
+//
+//  Created by Pidhi on 18/07/15.
+//  Copyright (c) 2015 Sample. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleFoundation : NSObject
+
+@end
---
+++ SampleFoundation/SampleFoundation.m
@@ -0,0 +1,13 @@
+//
+//  SampleFoundation.m
+//  SampleFoundation
+//
+//  Created by Pidhi on 18/07/15.
+//  Copyright (c) 2015 Sample. All rights reserved.
+//
+
+#import "SampleFoundation.h"
+
+@implementation SampleFoundation
+
+@end
